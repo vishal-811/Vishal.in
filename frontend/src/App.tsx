1c62849e4d2c23bc2971e7a5ca3f7316868fbbc9
vishalsharma <vishalssharma811@gmail.com>
2024-07-03 23:04:12 +0530

    fix typo error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { Layout } from "./pages/Layout"
 import { ThemeContext } from "./contexts/Theme"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 function App() {
      const [theme,setTheme]=useState("dark")
